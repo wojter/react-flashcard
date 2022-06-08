2a859dd0587442acf47af89de22f202ea4993803
Wojter <wojtektro2@gmail.com>
2022-06-09 01:12:52 +0200

    Add select quiz type, quiz card visuality

--- frontend/src/components/Quiz/QuizCard.js
+++ frontend/src/components/Quiz/QuizCard.js
@@ -1,10 +1,14 @@
 import { FaRegMeh, FaRegSmile, FaRegFrown } from 'react-icons/fa';
 import { useState } from 'react';
 import CardFlip from './CardFlip';
+import { Button, ProgressBar } from 'react-bootstrap';
 
 const QuizCard = (props) => {
 
     const [viewBack, setViewBack] = useState(false);
+
+    var actualIndex = props.actualIndex;
+    var totalNumber = props.totalNumber;
 
     const handleFlipCard = () => {
         setViewBack(!viewBack);
@@ -19,15 +23,24 @@ const QuizCard = (props) => {
     }
 
     return ( 
+        <div className='quiz-game'>
+            <ProgressBar
+             now={actualIndex}
+             label={actualIndex  + '/' + totalNumber }
+             max={totalNumber}
+             visuallyHidden />
         <div className='card-container'>
             <CardFlip
                 card={props.card}
                 isFlipped={viewBack}
                 flipCard={handleFlipCard}
             />
-            <button onClick={()=>handleKnow(1)}><FaRegSmile /> know well</button>
-            <button onClick={()=>handleKnow(2)}><FaRegMeh /> know medium</button>
-            <button onClick={()=>handleKnow(3)}><FaRegFrown /> don't know</button>
+            <div className='known-buttons'>
+            <Button className='known-button' variant="light" onClick={()=>handleKnow(1)}><FaRegSmile /> know well</Button>
+            <Button variant="light" onClick={() => handleKnow(2)}><FaRegMeh /> know medium</Button>
+            <Button variant="light" onClick={() => handleKnow(3)}><FaRegFrown /> don't know</Button>
+        </div>
+        </div>
         </div>
      );
 }
--- frontend/src/components/Quiz/SelectQuiz.js
+++ frontend/src/components/Quiz/SelectQuiz.js
@@ -1,9 +1,13 @@
 import { Link, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { MdArrowBackIosNew } from 'react-icons/md';
+import useStateWithCallbackLazy from 'use-state-with-callback';
+import { Button } from 'react-bootstrap';
 
 import axios from "../../axios";
 import QuizCard from "./QuizCard";
+
+
 
 const SelectQuiz = () => {
 
@@ -11,12 +15,14 @@ const SelectQuiz = () => {
 
     const [selectedDeck, setSelectedDeck] = useState([]);
     const [userCards, setUserCards] = useState([]);
+    const [userSelectedCards, setUserSelectedCards] = useState([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
-    const [selectedCard, setSelectedCard] = useState({frot: 'test', back: 'test back'});
+    const [selectedCard, setSelectedCard] = useState({frot: 'test front', back: 'test back'});
     const [displayStart, setDisplayStart] = useState(true);
     const [displayQuiz, setDisplayQuiz] = useState(false);
     const [displaySummary, setDisplaySummary] = useState(false);
-
+    const [disableCategory, setDisableCategory] = useState([false, false, false, false, false])
+    const [typeQuiz, setTypeQuiz] = useState(0);
 
     const getAllCards = () => {
         axios.get('/cards/' + _id)
@@ -24,7 +30,7 @@ const SelectQuiz = () => {
             // console.log(response);
             const allCards = response.data;
             // add data to our function state
-            setUserCards(allCards);
+            setUserCards(allCards,countNumberClass);
             console.log(allCards);
         })
         .catch(error => console.error('Error: ', error))
@@ -53,39 +59,92 @@ const SelectQuiz = () => {
     useEffect(() => {
         getDeck();
         getAllCards();
+        // countNumberClass();
     }, [])
+
+    const didMount = useRef(false);
+
+    useEffect(() => {
+        if (!didMount.current) {
+            didMount.current = true;
+            return;
+        }
+        startQuiz();
+    }, [userSelectedCards])
+
+    const didMount2 = useRef(false);
+
+    useEffect(()=>{
+        if (!didMount2.current) {
+            didMount2.current = true;
+            return;
+        }
+        countNumberClass();
+    }, [userCards])
 
     const quizFavourite = () => {
         const cards = [...userCards]
                         .filter(card => card.favourite === true);
-        setUserCards(cards);
+        setUserSelectedCards(cards);
     }
 
     const quizAll = () => {
-        setSelectedCard(userCards[0]);
+        setUserSelectedCards(userCards);
+        setTypeQuiz(1);
+        
     }
 
     const quizKnow = (props) => {
         const cards = [...userCards]
                         .filter(card => card.known === props);
-        setUserCards(cards);
+        setUserSelectedCards(cards);
     }
 
     const startQuiz = () => {
-
+        setDisplayStart(false);
+        setDisplayQuiz(true);
+        setDisplaySummary(false);
+        setSelectedCard(userSelectedCards[0]);
     }
     const nextQuestion = () => {
         updateCard(selectedCard);
-        if (selectedIndex + 1 >= userCards.length) {
+        if (selectedIndex + 1 >= userSelectedCards.length) {
             setDisplayStart(false);
             setDisplayQuiz(false);
             setDisplaySummary(true);
         } else {
             setSelectedIndex(selectedIndex + 1);
-            setSelectedCard(userCards[selectedIndex+1]);
+            setSelectedCard(userSelectedCards[selectedIndex+1]);
        }
+       console.log(userCards);
 
     }
+
+    const countNumberClass = () => {
+        var disCat = [...disableCategory];
+        if (userCards.length < 1) {
+            setDisableCategory(disCat);
+            console.log(disCat);
+            return;
+        } else {
+            disCat[0]=true;
+        }
+        if (userCards.filter(x => x.favourite === true).length > 0) {
+            disCat[1] = true;
+        }
+        if (userCards.filter(x => x.known === 1).length > 0) {
+            disCat[2] = true;
+        }
+        if (userCards.filter(x => x.known === 2).length > 0) {
+            disCat[3] = true;
+        }
+        if (userCards.filter(x => x.known === 3).length > 0) {
+            disCat[4] = true;
+        }
+        setDisableCategory(disCat);
+        //console.log(disCat);
+    }
+
 
     return ( 
         <div>
@@ -95,18 +154,30 @@ const SelectQuiz = () => {
                 </Link>
             <h2>{ selectedDeck.title }</h2>
             </div>
-            {displayStart}
-            <div className="startButtons">
-                <button onClick={quizAll}>All</button>
-                <button onClick={quizFavourite}>Favourites</button>
-                <button onClick={()=>quizKnow(1)}>Know well</button>
-                <button onClick={()=>quizKnow(2)}>Know Medium</button>
-                <button onClick={()=>quizKnow(3)}>Dont Know</button>
-            </div>
+            {displayStart && (
+            <div className="quiz-start">
+                <h3>Select from which category you want to learn</h3>
+            <div className="startButtons d-grid gap-2 " >
+                { disableCategory[0] ? <Button variant="light" onClick={quizAll}>All</Button> : <Button variant="light" disabled onClick={quizAll}>All</Button> }
+                { disableCategory[1] ? <Button variant="light" onClick={quizFavourite}>Favourites</Button> : <Button variant="light" disabled onClick={quizFavourite}>Favourites</Button> }
+                { disableCategory[2] ? <Button variant="light" onClick={()=>quizKnow(1)}>Know well</Button> : <Button variant="light" disabled onClick={()=>quizKnow(1)}>Know well</Button> }
+                { disableCategory[3] ? <Button variant="light" onClick={()=>quizKnow(2)}>Know Medium</Button> : <Button variant="light" disabled onClick={()=>quizKnow(2)}>Know Medium</Button> }
+                { disableCategory[4] ? <Button variant="light" onClick={()=>quizKnow(3)}>Dont Know</Button> : <Button variant="light" disabled onClick={()=>quizKnow(3)}>Dont Know</Button> }
+            </div> 
+            </div>  
+            )}
+            {displayQuiz && (
             <QuizCard
                 card={selectedCard}
                 goNext={nextQuestion}
-            />
+                actualIndex={selectedIndex}
+                totalNumber={userSelectedCards.length}
+            />  
+            )}
+            { displaySummary && ( <div/>)}
+
+
+
         </div>
      );
 }
